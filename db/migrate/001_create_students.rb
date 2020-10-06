dc850d7a9f1392b166ebaf8e0630925180e92ee8
David Charles <ratlover03@gmail.com>
2020-10-06 22:40:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table students do |t|
+    create_table :students do |t|
       t.string :first_name
       t.sting :last_name
+      
+      t.timestamps null: false
     end
   end
 end
